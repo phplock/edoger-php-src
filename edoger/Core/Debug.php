890c90e5adc85eba325b04639afbc5728de242c3
REENT <zreent@yeah.net>
2016-08-01 23:25:06 +0800

    增加基础功能

--- edoger/Core/Debug.php
+++ edoger/Core/Debug.php
@@ -32,54 +32,204 @@
 namespace Edoger\Core;
 
 use Edoger\Core\Log\Logger;
+use Edoger\Interfaces\EdogerExceptionInterface;
+use ErrorException;
 
 /**
+ * ================================================================================
  * 
+ * ================================================================================
  */
 final class Debug
 {
+	/**
+	 * ----------------------------------------------------------------------------
+	 * What is it ?
+	 * ----------------------------------------------------------------------------
+	 *
+	 * @var type
+	 */
+	private static $kernel;
+
+	/**
+	 * ----------------------------------------------------------------------------
+	 * What is it ?
+	 * ----------------------------------------------------------------------------
+	 *
+	 * @var type
+	 */
+	private static $hook = null;
+
+	/**
+	 * ----------------------------------------------------------------------------
+	 * What is it ?
+	 * ----------------------------------------------------------------------------
+	 *
+	 * @var type
+	 */
+	private static $logger = null;
+
+	private static $logCache = [];
+
+	/**
+	 * ----------------------------------------------------------------------------
+	 * What is it ?
+	 * ----------------------------------------------------------------------------
+	 *
+	 * @var type
+	 */
 	private static $map = [
-		E_ERROR             => Logger::CRITICAL,
-		E_WARNING           => Logger::WARNING,
-		E_PARSE             => Logger::ALERT,
-		E_NOTICE            => Logger::NOTICE,
-		E_CORE_ERROR        => Logger::CRITICAL,
-		E_CORE_WARNING      => Logger::WARNING,
-		E_COMPILE_ERROR     => Logger::ALERT,
-		E_COMPILE_WARNING   => Logger::WARNING,
-		E_USER_ERROR        => Logger::ERROR,
-		E_USER_WARNING      => Logger::WARNING,
-		E_USER_NOTICE       => Logger::NOTICE,
-		E_STRICT            => Logger::NOTICE,
-		E_RECOVERABLE_ERROR => Logger::ERROR,
-		E_DEPRECATED        => Logger::NOTICE,
-		E_USER_DEPRECATED   => Logger::NOTICE
+		E_ERROR             => Logger::LEVEL_CRITICAL,
+		E_WARNING           => Logger::LEVEL_WARNING,
+		E_PARSE             => Logger::LEVEL_ALERT,
+		E_NOTICE            => Logger::LEVEL_NOTICE,
+		E_CORE_ERROR        => Logger::LEVEL_CRITICAL,
+		E_CORE_WARNING      => Logger::LEVEL_WARNING,
+		E_COMPILE_ERROR     => Logger::LEVEL_ALERT,
+		E_COMPILE_WARNING   => Logger::LEVEL_WARNING,
+		E_USER_ERROR        => Logger::LEVEL_ERROR,
+		E_USER_WARNING      => Logger::LEVEL_WARNING,
+		E_USER_NOTICE       => Logger::LEVEL_NOTICE,
+		E_STRICT            => Logger::LEVEL_NOTICE,
+		E_RECOVERABLE_ERROR => Logger::LEVEL_ERROR,
+		E_DEPRECATED        => Logger::LEVEL_NOTICE,
+		E_USER_DEPRECATED   => Logger::LEVEL_NOTICE
 	];
-
-	private static $registered = false;
 	
-	public function __construct()
+	/**
+	 * ----------------------------------------------------------------------------
+	 * What is it ?
+	 * ----------------------------------------------------------------------------
+	 *
+	 * @return type
+	 */
+	public function __construct(Kernel &$kernel)
 	{
-		if (!self::$registered) {
-			self::$registered = true;
+		self::$kernel = &$kernel;
+	}
 
-			set_error_handler([$this, 'ErrorHandler']);
-			set_exception_handler([$this, 'ExceptionHandler']);
+	/**
+	 * ----------------------------------------------------------------------------
+	 * What is it ?
+	 * ----------------------------------------------------------------------------
+	 *
+	 * @return type
+	 */
+	public function register()
+	{
+		static $registered = false;
+
+		if (!$registered) {
+			$registered = true;
+
+			set_error_handler([$this, '_ErrorHandler']);
+			set_exception_handler([$this, '_ExceptionHandler']);
+			register_shutdown_function([$this, '_ShutdownHandler']);
+		}
+
+		return $registered;
+	}
+
+	public function bindLogger(Logger $logger)
+	{
+		self::$logger = $logger;
+		if (!empty(self::$logCache)) {
+			foreach (self::$logCache as $log) {
+				self::$logCache -> log($log[0], $log[1]);
+			}
+			self::$logCache = [];
 		}
 	}
 
-	public function stop()
+	public function bindHook($hook)
 	{
-
+		self::$hook = $hook;
 	}
 
-	public function ErrorHandler(int $code, string $message, string $file = '', int $line = 0, array $context = [])
+	private function executeHandler(string $message, int $level, int $code,, string $file, int $line)
 	{
-
+		if ($level <= Logger::LEVEL_ERROR) {
+			$this -> _ExceptionHandler(
+				new ErrorException($message, $level, $code, $file, $line)
+				);
+		} else {
+			$message = "{$message} at {$file} line {$line}";
+			if (self::$logger) {
+				self::$logger -> log($level, $message);
+			} else {
+				self::$logCache[] = [$level, $message];
+			}
+		}
 	}
 
-	public function ExceptionHandler($exception)
+	/**
+	 * ----------------------------------------------------------------------------
+	 * What is it ?
+	 * ----------------------------------------------------------------------------
+	 *
+	 * @return type
+	 */
+	public function _ErrorHandler(int $code, string $message, string $file = '', int $line = 0)
 	{
+		$this -> executeHandler(
+			$message,
+			self::$map[$code] ?? Logger::LEVEL_CRITICAL,
+			$code,
+			$file,
+			$line
+			);
+	}
 
+	/**
+	 * ----------------------------------------------------------------------------
+	 * What is it ?
+	 * ----------------------------------------------------------------------------
+	 *
+	 * @return type
+	 */
+	public function _ExceptionHandler($e)
+	{
+		if ($e instanceof EdogerExceptionInterface) {
+			$level 	= $e -> getLevel();
+			$log 	= $e -> getLog();
+		} else {
+			$level 	= self::$map[$e -> getCode()] ?? Logger::ERROR;
+			$log 	= "{$e -> getMessage()} at {$e -> getFile()} line {$e -> getLine()}";
+		}
+
+		if (self::$logger) {
+			self::$logger -> log($level, $log);
+		} else {
+			self::$logCache[] = [$level, $log];
+		}
+
+		if (self::$hook) {
+			self::$hook -> call();
+		}
+	}
+
+	/**
+	 * ----------------------------------------------------------------------------
+	 * What is it ?
+	 * ----------------------------------------------------------------------------
+	 *
+	 * @return type
+	 */
+	public function _ShutdownHandler()
+	{
+		$error = error_get_last();
+		if ($error) {
+			
+			//	Clear the last error.
+			error_clear_last();
+
+			$this -> executeHandler(
+				$error['message'],
+				self::$map[$error['type']] ?? Logger::CRITICAL,
+				$error['type'],
+				$error['file'],
+				$error['line']
+				);
+		}
 	}
 }
