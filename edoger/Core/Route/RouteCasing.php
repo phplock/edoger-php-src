ea4f710d195d8b6f2bd3c6956171fea524a39a32
REENT <zreent@yeah.net>
2016-09-19 23:38:15 +0800

    fix

--- edoger/Core/Route/RouteCasing.php
+++ edoger/Core/Route/RouteCasing.php
@@ -1,57 +1,113 @@
 <?php
-/*
- +-----------------------------------------------------------------------------+
- | Edoger PHP Framework (EdogerPHP)                                            |
- +-----------------------------------------------------------------------------+
- | Copyright (c) 2014 - 2016 QingShan Luo                                      |
- +-----------------------------------------------------------------------------+
- | The MIT License (MIT)                                                       |
- |                                                                             |
- | Permission is hereby granted, free of charge, to any person obtaining a     |
- | copy of this software and associated documentation files (the “Software”),  |
- | to deal in the Software without restriction, including without limitation   |
- | the rights to use, copy, modify, merge, publish, distribute, sublicense,    |
- | and/or sell copies of the Software, and to permit persons to whom the       |
- | Software is furnished to do so, subject to the following conditions:        |
- |                                                                             |
- | The above copyright notice and this permission notice shall be included in  |
- | all copies or substantial portions of the Software.                         |
- |                                                                             |
- | THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,             |
- | EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF          |
- | MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.      |
- | IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, |
- | DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR       |
- | OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE   |
- | USE OR OTHER DEALINGS IN THE SOFTWARE.                                      |
- +-----------------------------------------------------------------------------+
- |  License: MIT                                                               |
- +-----------------------------------------------------------------------------+
- |  Authors: QingShan Luo <shanshan.lqs@gmail.com>                             |
- +-----------------------------------------------------------------------------+
+/**
+ *+----------------------------------------------------------------------------+
+ *| Edoger PHP Framework (Edoger)                                              |
+ *+----------------------------------------------------------------------------+
+ *| Copyright (c) 2014 - 2016 QingShan Luo (Reent)                             |
+ *+----------------------------------------------------------------------------+
+ *| The MIT License (MIT)                                                      |
+ *|                                                                            |
+ *| Permission is hereby granted, free of charge, to any person obtaining a    |
+ *| copy of this software and associated documentation files (the “Software”), |
+ *| to deal in the Software without restriction, including without limitation  |
+ *| the rights to use, copy, modify, merge, publish, distribute, sublicense,   |
+ *| and/or sell copies of the Software, and to permit persons to whom the      |
+ *| Software is furnished to do so, subject to the following conditions:       |
+ *|                                                                            |
+ *| The above copyright notice and this permission notice shall be included in |
+ *| all copies or substantial portions of the Software.                        |
+ *|                                                                            |
+ *| THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,            |
+ *| EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF         |
+ *| MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.     |
+ *| IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,|
+ *| DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR      |
+ *| OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE  |
+ *| USE OR OTHER DEALINGS IN THE SOFTWARE.                                     |
+ *+----------------------------------------------------------------------------+
+ *| License: MIT                                                               |
+ *+----------------------------------------------------------------------------+
+ *| Authors: QingShan Luo <shanshan.lqs@gmail.com>                             |
+ *+----------------------------------------------------------------------------+
+ *| Link: https://www.edoger.com/                                              |
+ *+----------------------------------------------------------------------------+
  */
 namespace Edoger\Core\Route;
 
 /**
- * ================================================================================
- *
- *
- * ================================================================================
+ *==============================================================================
+ * 
+ *==============================================================================
  */
 final class RouteCasing
 {
+	/**
+	 * -------------------------------------------------------------------------
+	 * [$filter description]
+	 * -------------------------------------------------------------------------
+	 * 
+	 * @var [type]
+	 */
 	private $filter;
+	
+	/**
+	 * -------------------------------------------------------------------------
+	 * [$middleware description]
+	 * -------------------------------------------------------------------------
+	 * 
+	 * @var [type]
+	 */
 	private $middleware;
+	
+	/**
+	 * -------------------------------------------------------------------------
+	 * [$middleware description]
+	 * -------------------------------------------------------------------------
+	 * 
+	 * @var [type]
+	 */
 	private $isMatch;
+	
+	/**
+	 * -------------------------------------------------------------------------
+	 * [$middleware description]
+	 * -------------------------------------------------------------------------
+	 * 
+	 * @var [type]
+	 */
 	private $domain;
+	
+	/**
+	 * -------------------------------------------------------------------------
+	 * [$middleware description]
+	 * -------------------------------------------------------------------------
+	 * 
+	 * @var [type]
+	 */
 	private $port;
+	
+	/**
+	 * -------------------------------------------------------------------------
+	 * [$middleware description]
+	 * -------------------------------------------------------------------------
+	 * 
+	 * @var [type]
+	 */
 	private $scheme;
+	
+	/**
+	 * -------------------------------------------------------------------------
+	 * [$middleware description]
+	 * -------------------------------------------------------------------------
+	 * 
+	 * @var [type]
+	 */
 	private $xhr;
 	
 	/**
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * [__construct description]
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * 
 	 * @param string $domain      [description]
 	 * @param int    $port        [description]
@@ -82,9 +138,9 @@ final class RouteCasing
 	}
 
 	/**
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * [filter description]
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * 
 	 * @param  array  $filter [description]
 	 * @return [type]         [description]
@@ -99,9 +155,9 @@ final class RouteCasing
 	}
 
 	/**
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * [middleware description]
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * 
 	 * @param  array  $middleware [description]
 	 * @return [type]             [description]
@@ -116,9 +172,9 @@ final class RouteCasing
 	}
 
 	/**
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * [scheme description]
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * 
 	 * @param  string $scheme [description]
 	 * @return [type]         [description]
@@ -136,9 +192,9 @@ final class RouteCasing
 	}
 
 	/**
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * [domain description]
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * 
 	 * @param  string $domain [description]
 	 * @return [type]         [description]
@@ -156,9 +212,9 @@ final class RouteCasing
 	}
 
 	/**
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * [xhr description]
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * 
 	 * @return [type] [description]
 	 */
@@ -173,9 +229,9 @@ final class RouteCasing
 
 
 	/**
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * [hook description]
-	 * ----------------------------------------------------------------------------
+	 * -------------------------------------------------------------------------
 	 * 
 	 * @param  callable $hook [description]
 	 * @return [type]         [description]
