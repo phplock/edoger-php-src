788bfc06a5bc6e9c4706c6052ea1b5f235fa9378
REENT <zreent@yeah.net>
2016-10-19 18:05:26 +0800

    Update Config file.

--- config/edoger.config.php
+++ config/edoger.config.php
@@ -17,15 +17,20 @@
 $conf = [];
 // -------------------------------------------------------------------------------------------------
 
+// This string is used to encrypt your security cookie, please do not leak it.
+// You should always change it. If you are the first installation, please replace it immediately.
 $conf['cookie_secret_key']		= 'ORqCwo4wmMNJhutnB4CSYq6m9KqQAvoH';
 
+// This is the default controller name, action name and view name for the system.
 $conf['default_controller']		= 'index';
 $conf['default_action']			= 'index';
 $conf['default_view']			= 'index';
 
+// Controller and model namespace name.
 $conf['model_namespace']		= '\\App\\Model\\';
 $conf['controller_namespace']	= '\\App\\Controller\\';
 
+// View directory and view extension name.
 $conf['view_directory']			= APP_PATH.'/View';
 $conf['view_extension_name']	= 'phtml';
 
