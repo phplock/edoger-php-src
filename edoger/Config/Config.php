c60cdac346aa9251ea78346c40735fb7c557733f
edoger <shanshan.lqs@gmail.com>
2016-11-24 00:00:42 +0800

    Add config files.

---
+++ edoger/Config/Config.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *+------------------------------------------------------------------------------------------------+
+ *| Edoger PHP Framework                                                                           |
+ *+------------------------------------------------------------------------------------------------+
+ *| A simple and efficient PHP framework.                                                          |
+ *+------------------------------------------------------------------------------------------------+
+ *| @license   MIT                                                                                 |
+ *| @link      https://www.edoger.com/                                                             |
+ *| @copyright Copyright (c) 2014 - 2016, QingShan Luo                                             |
+ *+------------------------------------------------------------------------------------------------+
+ *| @author    Qingshan Luo <shanshan.lqs@gmail.com>                                               |
+ *+------------------------------------------------------------------------------------------------+
+ */
+namespace Edoger\Config;
+
+use Edoger\Foundation\Config\ConfigInterface;
+
+class Config implements ConfigInterface
+{
+    protected $items;
+
+    public function __construct(array $items)
+    {
+        $this->items = $items;
+    }
+
+    public function get($key, $default = null)
+    {
+        if (isset($this->items[$key])) {
+            return $this->items[$key];
+        } else {
+            return $default;
+        }
+    }
+
+    public function set($key, $value, $cover = true)
+    {
+        if ($cover || !isset($this->items[$key])) {
+            $this->items[$key] = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function has($key)
+    {
+        return isset($this->items[$key]);
+    }
+
+    public function all()
+    {
+        return $this->items;
+    }
+}
---
+++ edoger/Foundation/Config/ConfigInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *+------------------------------------------------------------------------------------------------+
+ *| Edoger PHP Framework                                                                           |
+ *+------------------------------------------------------------------------------------------------+
+ *| A simple and efficient PHP framework.                                                          |
+ *+------------------------------------------------------------------------------------------------+
+ *| @license   MIT                                                                                 |
+ *| @link      https://www.edoger.com/                                                             |
+ *| @copyright Copyright (c) 2014 - 2016, QingShan Luo                                             |
+ *+------------------------------------------------------------------------------------------------+
+ *| @author    Qingshan Luo <shanshan.lqs@gmail.com>                                               |
+ *+------------------------------------------------------------------------------------------------+
+ */
+namespace Edoger\Foundation\Config;
+
+interface ConfigInterface
+{
+    public function get($key, $default = null);
+    public function set($key, $value, $cover = true);
+    public function has($key);
+    public function all();
+}
