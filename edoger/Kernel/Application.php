00d2cd7f126f112eaa58c4ae7f174544300679e7
edoger <shanshan.lqs@gmail.com>
2016-11-29 21:43:10 +0800

    Update application class.

--- edoger/Kernel/Application.php
+++ edoger/Kernel/Application.php
@@ -14,22 +14,70 @@
  */
 namespace Edoger\Kernel;
 
+use App\Bootstrap;
 use Edoger\Config\Config;
+use Edoger\Exceptions\RuntimeException;
+use Edoger\Http\Response;
 
 class Application extends Kernel
 {
     const VERSION = '1.0.0';
 
+    protected static $booted = false;
+
     public function __construct(Config $config)
     {
         static::$application   = &$this;
         static::$configuration = $config;
-
-        $this->instance(Application::class, $this);
-        $this->instance(get_class($config), $config);
     }
 
-    public function version()
+    public function singleton($abstract, $alias)
+    {
+        $alias = strtolower($alias);
+        $name  = ltrim($abstract, '\\');
+        if (isset(static::$instances[$name])) {
+            return $this->alias($alias, $name)->get($alias);
+        } else {
+            return $this->alias($alias, $name)->set($name, new $abstract($this));
+        }
+    }
+
+    public function bootstrap()
+    {
+        if (static::$booted) {
+            return $this;
+        }
+
+        static::$booted = true;
+
+        $methods = get_class_methods(Bootstrap::class);
+        if (!empty($methods)) {
+            $bootstrap = new Bootstrap();
+            foreach ($methods as $method) {
+                if (substr($method, 0, 4) === 'init') {
+                    $bootstrap->$method($this);
+                }
+            }
+        }
+
+        return $this;
+    }
+
+    public function capture($abstract, $dependent)
+    {
+        $object = new $abstract($this);
+        if ($object instanceof Response) {
+            $object->collect($dependent);
+        } else {
+            throw new RuntimeException(
+                'The Application capture targets must be integrated with the Response class.'
+            );
+        }
+
+        return $object;
+    }
+
+    public static function version()
     {
         return static::VERSION;
     }
--- edoger/Kernel/Kernel.php
+++ edoger/Kernel/Kernel.php
@@ -20,8 +20,41 @@ class Application
 {
     protected static $application;
     protected static $configuration;
+    protected static $instances = [];
+    protected static $aliases   = [];
+    protected static $helpers   = [];
 
-    protected static $helpers = [];
+    protected function has($abstract)
+    {
+        return isset(static::$aliases[$abstract]);
+    }
+
+    protected function get($abstract)
+    {
+        if ($this->has($abstract)) {
+            return static::$instances[
+                static::$aliases[
+                    $abstract
+                ]
+            ];
+        } else {
+            throw new RuntimeException(
+                "The kernel extension instance {$abstract} does not exist or is not loaded."
+            );
+        }
+    }
+
+    protected function set($abstract, $instance)
+    {
+        static::$instances[$abstract] = $instance;
+        return $instance;
+    }
+
+    protected function alias($alias, $abstract)
+    {
+        static::$aliases[$alias] = $abstract;
+        return $this;
+    }
 
     public static function app()
     {
@@ -45,10 +78,20 @@ class Application
             static::$helpers[$helper] = true;
             require $file;
             return true;
+        } else {
+            throw new RuntimeException(
+                "The kernel helper library {$helper} does not exist."
+            );
         }
+    }
 
-        throw new RuntimeException(
-            "Helper library {$helper} does not exist."
-        );
+    public function __get($extension)
+    {
+        return $this->get(strtolower($extension));
+    }
+
+    public function __isset($extension)
+    {
+        return $this->has(strtolower($extension));
     }
 }
